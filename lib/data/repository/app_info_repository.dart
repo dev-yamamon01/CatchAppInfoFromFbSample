3d19c939ed07e58a1a79a5aa111c72616ca01482
dev-yamamon01 <dev.yamamon01@gmail.com>
2025-08-16 23:54:10 +0900

    feat:とりあえずversion監視のコードを作成

--- lib/data/repository/app_info_repository.dart
+++ lib/data/repository/app_info_repository.dart
@@ -1,5 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-class AppInfoRepository{
+final appInfoRepositoryProvider = Provider((ref) => AppInfoRepository());
+
+final appInfoStreamProvider =
+StreamProvider<Map<String, dynamic>?>((ref) {
+  return ref.watch(appInfoRepositoryProvider).listenAppInfo();
+});
+
+
+class AppInfoRepository {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
   ///firestoreのapp/appInfoドキュメントのスナップショットをリアルタイム取得するメソッド
+  Stream<Map<String, dynamic>?> listenAppInfo() {
+    return _db.collection('app').doc('appInfo').snapshots().map(
+          (snapshot) => snapshot.data(),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:catch_app_info_from_fb_sample/presentation/view/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:catch_app_info_from_fb_sample/data/repository/app_info_repository.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +17,41 @@ void main() async{
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    // グローバルに監視
+    ref.listen(appInfoStreamProvider, (prev, next) {
+      next.whenData((data) {
+        print('検知');
+        if (data == null) return;
+
+        // 例えばバージョンチェックして強制アップデートダイアログを表示
+        final latestVersion = data['latestVersion'] as String?;
+        if (latestVersion != null && latestVersion != '1.0.0') {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (_) => AlertDialog(
+              title: const Text("アップデート必須"),
+              content: const Text("新しいバージョンに更新してください。"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    // AppStoreやPlayStoreのURLへ遷移させる
+                  },
+                  child: const Text("更新する"),
+                ),
+              ],
+            ),
+          );
+        }
+      });
+    });
+
     return MaterialApp(
       home: const HomePage(),
     );
